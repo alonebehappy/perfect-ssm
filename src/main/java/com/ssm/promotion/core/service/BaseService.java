3c20264de9f7bda29b7fe3f861470920d5618f87
alonebehappy <39368859+alonebehappy@users.noreply.github.com>
2019-10-23 11:26:58 +0800

    Create BaseService.java

---
+++ src/main/java/com/ssm/promotion/core/service/BaseService.java
@@ -0,0 +1,12 @@
+public interface BaseService<ID extends Serializable, T extends AbstractEntity, I extends BaseDao<?, ?, ?>>
+{
+	boolean save(T u);
+	
+	boolean deleteById(ID id);
+	
+	boolean update(T u);
+	
+	T getById(ID id);
+	
+	List<T> getAll();
+}
