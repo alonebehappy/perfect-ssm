32ad92202f4f99d71f282eebccc28e2a3c2b37f9
alonebehappy <39368859+alonebehappy@users.noreply.github.com>
2019-11-07 18:06:46 +0800

    Update BaseService.java

--- src/main/java/com/ssm/promotion/core/service/BaseService.java
+++ src/main/java/com/ssm/promotion/core/service/BaseService.java
@@ -1,12 +1,18 @@
-public interface BaseService<ID extends Serializable, T extends AbstractEntity, I extends BaseDao<?, ?, ?>>
-{
-	boolean save(T u);
-	
-	boolean deleteById(ID id);
-	
-	boolean update(T u);
-	
-	T getById(ID id);
-	
-	List<T> getAll();
+public interface BaseService<ID extends Serializable, T extends AbstractEntity<ID>> {
+
+	int deleteByPrimaryKey(ID id);
+
+	int insert(T record);
+
+	int insertSelective(T record);
+
+	T selectByPrimaryKey(ID id);
+
+	int updateByPrimaryKeySelective(T record);
+
+	int updateByPrimaryKey(T record);
+
+	List<T> selectList(T record);
+
+	PageInfo<T> getPageData(T record, int pageNum, int pageSize);
 }
